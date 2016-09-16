ef754b9b1198694e046abc91b401af56e44a7051
obskyr <powpowd@gmail.com>
2016-09-16 10:44:38 +0200

    Completed setup.py.
    Whoops, forgot to commit it before.

--- setup.py
+++ setup.py
@@ -3,10 +3,41 @@
 
 from setuptools import setup
 
-VERSION = '0.0.1-dev'
+VERSION = '1.0.0'
+
+REQUIREMENTS = [
+    "pillow >= 3.3.1"
+]
+
+with open("readme.rst", 'r') as f:
+    long_description = f.read()
 
 setup(
     name="colorgram.py",
     version=VERSION,
-    packages=['colorgram']
+    install_requires=REQUIREMENTS,
+    packages=['colorgram'],
+    author="Samuel Messner",
+    author_email="powpowd@gmail.com",
+    url="https://github.com/obskyr/colorgram.py",
+    download_url="https://github.com/obskyr/colorgram.py/tarball/v" + VERSION,
+    description="A Python module for extracting colors from images. Get a palette of any picture!",
+    long_description=long_description,
+    license="MIT",
+    keywords="color colors palette extract image picture",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
 )
