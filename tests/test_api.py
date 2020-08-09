50e3c974f8cb0bdf1af8e8fb6b3912d4ee3ed0dc
Nathan LeRoy <NLeRoy917@gmail.com>
2020-08-09 14:23:03 -0400

    colorgram now independent of source - detects if bytes object (for url)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import colorgram
+from colorgram import colorgram
 from PIL import Image
+import requests
 
 def test_extract_from_file():
     colorgram.extract('data/test.png', 1)
 
 def test_extract_from_image_object():
     image = Image.open('data/test.png')
-    colorgram.extract(image, 1)
+    colors = colorgram.extract(image, 1)
 
 def test_extract_from_url():
     url = 'https://camo.githubusercontent.com/ca5e835b6671e2eb15679c13af834927f3d4d26e/687474703a2f2f692e696d6775722e636f6d2f4265526561524d2e706e67'
-    colorgram.extract(url,url=True)
+    res = requests.get(url)
+    colorgram.extract(res.content, 1)
 
 def test_color_access():
     color = colorgram.Color(255, 151, 210, 0.15)
